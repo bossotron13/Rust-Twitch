36b3d788ee55085fc1460c224741b63723f103a2
Bossotron <bossotron13@gmail.com>
2021-07-05 17:17:01 -0400

    Fixed Live XPath

--- API.py
+++ API.py
@@ -6,22 +6,22 @@ import time
 class API:
     def __init__(self, driver):
         self.TwitchPath = "https://twitch.tv/"
-        self.Live = '//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/div/div/a/div[2]/div/div/div'
+        self.Live = '//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/div/div/div/a/div[2]/div/div/div'
         self.WatchBc = '//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div/div/div[5]/div/div[3]/button/div/div'
         self.isRust = '//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[1]/a/span'
         self.Cookie = cookies
         self.driver = driver
         self.LoggingIn = False
         self.StreamerList = {
-            "imPheetus": 0,
-            "BaboAbe": 0,
-            "fuslie": 0,
-            "itsBigChase": 0,
-            "peterparkTV": 0,
-            "Enardo": 0,
-            "Shiphtur": 0,
-            "spoonkid": 0,
-            "Masayoshi": 0
+            "ShackyHD": 0,
+            "starsmitten": 0,
+            "itsRyanHiga": 0,
+            "Frost_": 0,
+            "kkatamina": 0,
+            "xChocoBars": 0,
+            "QuarterJade": 0,
+            "iiTzTimmy": 0,
+            "Natsumii": 0
         }
 
         self.Threads()
@@ -46,13 +46,14 @@ class API:
     
     def isLoggedin(self) -> bool:
         for x in self.driver.get_cookies():
-            if x["value"].lower() == "Twitch name here":
+            if x["value"].lower() == "Twitch name here ALL LOWER CASE!":
                 return True
         return False
 
     def gotoStreamer(self, streamer: str) -> bool:
         self.driver.get(self.TwitchPath + streamer)
         time.sleep(1)
+        print(self.check_if_exists(self.Live))
         if self.check_if_exists(self.Live) and self.driver.find_element_by_xpath(self.isRust).text.lower() == "rust":
             return True
         return False
